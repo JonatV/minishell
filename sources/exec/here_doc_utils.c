65eafa5504f8555263431054ef78aad8aa99b367
JonatV <mrangelsjon@gmail.com>
2024-10-28 23:00:49 +0100

    fix: double shell prompt thankls to readline

--- sources/exec/here_doc_utils.c
+++ sources/exec/here_doc_utils.c
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 11:28:05 by jveirman          #+#    #+#             */
-/*   Updated: 2024/10/27 10:56:46 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/10/28 22:59:20 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,55 +19,42 @@ int	is_here_doc_available(t_shell *shell, int i)
 	return (1);
 }
 
-static char	*ft_delimiter_hunter(int fd, char *ret, char *to_find)
+// todo - clean - check for eof and ctrl+c
+static char *ft_delimiter_hunter(char *ret, char *to_find)
 {
 	char	*buf;
-	ssize_t	n_byt;
-	char	*temp_text;
+	char	*temp;
 
-	buf = malloc(2 * sizeof(char));
-	if (!buf)
-		return (NULL);
-	n_byt = 1;
-	while (!ft_strnstr(ret, to_find, ft_strlen(to_find)) && (n_byt != 0))
+	while (1)
 	{
-		n_byt = read(fd, buf, 1);
-		if (n_byt == -1)
+		buf = readline("> ");
+		if (!buf)
+			return (free(ret), NULL);
+		if (ft_strcmp(buf, to_find) == 0)
 		{
-			if (ret)
-				free(ret);
 			free(buf);
-			return (NULL);
+			break;
 		}
-		buf[n_byt] = '\0';
-		temp_text = ret;
+		temp = ret;
 		ret = ft_strjoin(ret, buf);
-		free(temp_text);
+		free(temp);
+		temp = ret;
+		ret = ft_strjoin(ret, "\n");
+		free(temp);
+		free(buf);
 	}
-	free(buf);
 	return (ret);
 }
 
 char	*to_the_delimiter(char *to_find)
 {
-	int		fd;
 	char	*text_stored;
-	char	*text_trimmed;
 
-	fd = STDIN_FILENO;
 	text_stored = NULL;
-	text_trimmed = NULL;
 	if (!to_find || !to_find[0])
 		return (NULL);
-	text_stored = ft_delimiter_hunter(fd, text_stored, to_find);
+	text_stored = ft_delimiter_hunter(text_stored, to_find);
 	if (!text_stored)
-		return (NULL);
-	text_trimmed = ft_substr(text_stored, 0, \
-	ft_strlen(text_stored) - ft_strlen(to_find));
-	free(text_stored);
-	if (text_trimmed[0])
-		return (text_trimmed);
-	else
-		free(text_trimmed);
-	return (NULL);
+		text_stored = ft_strdup("");
+	return (text_stored);
 }
