164bf49b362b9a2ea11598649335914d296a1b2c
JonatV <mrangelsjon@gmail.com>
2024-10-30 20:28:41 +0100

    fix: complete previous commit

--- sources/shell/exit.c
+++ sources/shell/exit.c
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 16:40:08 by jveirman          #+#    #+#             */
-/*   Updated: 2024/10/27 22:12:47 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/10/30 17:41:38 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,19 @@ static void	free_machine(t_shell *shell)
 void	clean(char *str, t_shell *shell)
 {
 	if (str)
-		ft_putstr(str);
+		ft_putstr_fd(str, shell->current_fd_out);
 	free_machine(shell);
 }
 
 void	panic(char *str, t_shell *shell)
 {
-	ft_putstr(MSG_ERROR);
+	ft_putstr_fd(MSG_ERROR, STDERR_FILENO);
 	if (str)
 	{
-		ft_putstr(" : ");
-		ft_putstr(str);
+		ft_putstr_fd(" : ", STDERR_FILENO);
+		ft_putstr_fd(str, STDERR_FILENO);
 	}
-	ft_putstr("\n");
+	ft_putstr_fd("\n", STDERR_FILENO);
 	if (shell != NULL)
 		free_machine(shell);
 	exit(EXIT_FAILURE);
