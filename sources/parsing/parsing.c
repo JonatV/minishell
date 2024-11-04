216175e6b157e5a1fc2f9a41250caa3391c0346d
JonatV <mrangelsjon@gmail.com>
2024-11-05 00:45:43 +0100

    fix: refactor regroup token litteral processus and fix the leak from skipped tokens

--- sources/parsing/parsing.c
+++ sources/parsing/parsing.c
@@ -6,41 +6,49 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 11:21:39 by haroldsorel       #+#    #+#             */
-/*   Updated: 2024/11/04 02:17:47 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/11/05 00:41:47 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static bool	regroup_litteral_tokens(t_shell *shell)
+static bool concatenate_tokens(t_token *parent, t_token *tmp)
 {
-	t_token	*tmp;
-	t_token	*parent;
 	char	*content;
 	char	*str_temp;
+	t_token	*next_token;
+
+	content = ft_strdup(parent->content);
+	if (!content)
+		return (false);
+	while (tmp->next && (tmp->next->type == TOKEN_WORD || tmp->next->type == TOKEN_DOUBLE_QUOTE || tmp->next->type == TOKEN_SINGLE_QUOTE))
+	{
+		str_temp = ft_strjoin(content, tmp->next->content);
+		free(content);
+		if (!str_temp)
+			return (false);
+		content = str_temp;
+		next_token = tmp->next->next;
+		free(tmp->next->content);
+		free(tmp->next);
+		tmp->next = next_token;
+	}
+	free(parent->content);
+	parent->content = content;
+	return (true);
+}
+
+static bool	regroup_litteral_tokens(t_shell *shell)
+{
+	t_token	*parent;
 
 	parent = shell->tokens_list;
 	while (parent)
 	{
 		if (parent->type == TOKEN_WORD || parent->type == TOKEN_DOUBLE_QUOTE || parent->type == TOKEN_SINGLE_QUOTE)
 		{
-			content = ft_strdup(parent->content);
-			if (!content)
+			if (!concatenate_tokens(parent, parent))
 				return (false);
-			tmp = parent;
-			while (tmp->next && (tmp->next->type == TOKEN_WORD || tmp->next->type == TOKEN_DOUBLE_QUOTE || tmp->next->type == TOKEN_SINGLE_QUOTE))
-			{
-				str_temp = ft_strjoin(content, tmp->next->content);
-				free(content);
-				if (!str_temp)
-					return (false);
-				content = str_temp;
-				tmp->next->type = TOKEN_SKIP;
-				tmp = tmp->next;
-			}
-			free(parent->content);
-			parent->content = content;
-			parent->next = tmp->next;
 		}
 		parent = parent->next;
 	}
--- sources/token/handle_words.c
+++ sources/token/handle_words.c
@@ -6,64 +6,11 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 17:09:42 by jveirman          #+#    #+#             */
-/*   Updated: 2024/11/02 23:34:06 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/11/05 00:31:31 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-// bool	handle_word(char *buf, int *i, t_token **tokens_list)
-// {
-// 	int		start;
-// 	int		end;
-// 	char	*tmp;
-// 	t_token_type	type;
-// 	t_token	*pending_token;
-
-// 	start = *i;
-// 	if (buf[*i] == '"')
-// 	{
-// 		*i += 1;
-// 		while (buf[*i] && buf[*i] != '"')
-// 			*i += 1;
-// 		end = *i;
-// 		type = TOKEN_DOUBLE_QUOTE;
-// 		tmp = ft_substr(buf, start + 1, (end - start) - 1);
-// 		*i += 1;
-// 	}
-// 	else if (buf[*i] == 39)
-// 	{
-// 		*i += 1;
-// 		while (buf[*i] && buf[*i] != 39)
-// 			*i += 1;
-// 		end = *i;
-// 		type = TOKEN_SINGLE_QUOTE;
-// 		tmp = ft_substr(buf, start + 1, (end - start) - 1);
-// 		*i += 1;
-// 	}
-// 	else
-// 	{
-// 		while (buf[*i] && !ft_isspace(buf[*i]) && buf[*i] != '>' && buf[*i] != '<' && buf[*i] != '|')
-// 			*i += 1;
-// 		end = *i;
-// 		type = TOKEN_WORD;
-// 		tmp = ft_substr(buf, start, end - start);
-// 	}
-// 	if (!tmp)
-// 	{
-// 		free_tokens_list(*tokens_list);
-// 		return (false);
-// 	}
-// 	pending_token = create_token(type, &tmp);
-// 	if (!add_token(tokens_list, pending_token))
-// 	{
-// 		free_tokens_list(*tokens_list);
-// 		return (false);
-// 	}
-// 	return (true);
-// }
-
-
 
 static char	*extract_quoted(char *buf, int *i, char quote, int *type)
 {
@@ -110,7 +57,7 @@ void	handle_word(char *buf, int *i, t_token **tokens_list)
 		return ;
 	}
 	pending_token = create_token(type, &tmp);
-	if (!add_token(tokens_list, pending_token))
+	if (!pending_token || !add_token(tokens_list, pending_token))
 	{
 		free_tokens_list(tokens_list);
 		return ;
