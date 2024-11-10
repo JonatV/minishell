d70dc051f0eed0434be50e95a9e16e2d8a546920
JonatV <mrangelsjon@gmail.com>
2024-11-10 15:02:01 +0100

    refactor: change exit function to handle high number modulo 256

--- sources/built_in/exit.c
+++ sources/built_in/exit.c
@@ -6,17 +6,17 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 10:47:29 by jveirman          #+#    #+#             */
-/*   Updated: 2024/11/07 12:26:42 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/11/09 20:15:18 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static bool	is_non_numeric(t_shell *shell, char *str)
+static int	is_non_numeric(t_shell *shell, char *str)
 {
 	int i = 0;
 	if (!str)
-		return (false);
+		return (0);
 	if (str[i] == '-')
 		i++;
 	while (str[i])
@@ -24,33 +24,39 @@ static bool	is_non_numeric(t_shell *shell, char *str)
 		if (!ft_isdigit(str[i]) && str[i] != ' ')
 		{
 			mini_printf("minishell: exit: ", str, ": numeric argument required\n", shell->current_fd_out);
-			g_exit_status = 2;
-			return (true);
+			return (2);
 		}
 		i++;
 	}
-	return (false);
+	return (0);
 }
 
-static bool	non_numeric_found(t_shell *shell, int i)
+static int	non_numeric_found(t_shell *shell, int i)
 {
 	return (is_non_numeric(shell, shell->cmd_array[i].data[CMD_FLAG]) \
 			|| is_non_numeric(shell, shell->cmd_array[i].data[CMD_ARG]));
 }
 
-//todo clean function for the all minishell
-void	builtin_exit(t_shell *shell, int i)
+int	builtin_exit(t_shell *shell, int i)
 {
 	int	size;
-
+	int ret_value;
+	
+	ret_value = 0;
 	size = shell->cmd_array[i].num_flag + shell->cmd_array[i].num_arg;
 	ft_putstr_fd("exit\n", shell->current_fd_out);
 	if (size > 1)
 	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		g_exit_status = 1;
-		return ;
+		error_msg("exit: too many arguments");
+		return (1);
 	}
-	non_numeric_found(shell, i);
-	exit(g_exit_status);
+	ret_value = non_numeric_found(shell, i);
+	if (ret_value)
+		return (ret_value);
+	if (shell->cmd_array[i].data[CMD_FLAG])
+		ret_value = ft_atoi(shell->cmd_array[i].data[CMD_FLAG]);
+	else
+		ret_value = ft_atoi(shell->cmd_array[i].data[CMD_ARG]);
+	ret_value = ret_value % 256;
+	return (ret_value);
 }
